3f80672dc9837a06a3bda485018185a7f4109cf2
moti <moti@practitest.com>
2017-05-04 01:24:34 +0700

    moving tab to spot

--- app/background.js
+++ app/background.js
@@ -1,10 +1,16 @@
 chrome.commands.onCommand.addListener(function(command) {
   let move = 1
+  let newIndex = undefined
   if(command === "move-left") move = -1
+  if(command === "move-one") newIndex = 0
   chrome.tabs.query( {active: true, currentWindow: true},([currentTab]) => {
-    console.log(currentTab)
     if(currentTab){
-      chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: currentTab.index + move})
+      if(newIndex != undefined){
+        chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
+      }else{
+        chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: currentTab.index + move})
+      }
+
     }
   })
 
