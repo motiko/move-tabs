5792ce380642d51cb0e619b6bcd414c4111ab647
moti <moti@practitest.com>
2017-05-07 23:09:38 +0700

    change to ctrl + add cycle

--- app/background.js
+++ app/background.js
@@ -5,16 +5,16 @@ chrome.commands.onCommand.addListener(function(command) {
   if(command === "move-first") newIndex = 0
   if(command === "move-last") newIndex = -1
   chrome.tabs.query( {active: true, currentWindow: true},([currentTab]) => {
-
+    if(newIndex != undefined){
+      chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
+      return
+    }
     if(currentTab){
-      if(newIndex != undefined){
-        console.log(newIndex)
-        chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
-      }else{
-        chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: currentTab.index + move})
-      }
-
+      chrome.tabs.getAllInWindow(currentTab.windowId, (tabs)=> {
+          newIndex = currentTab.index + move
+          if(newIndex >= tabs.length) newIndex = 0
+          chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
+      })
     }
   })
-
 })
