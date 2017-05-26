bc5b281a43c2b6e40fc3a47dd5adfbf3952ae620
moti <moti.kor@gmail.com>
2017-05-27 00:30:53 +0700

    use browser-polyfill

--- chrome/background.js
+++ chrome/background.js
@@ -1,19 +1,19 @@
-chrome.commands.onCommand.addListener(function(command) {
+browser.commands.onCommand.addListener(function(command) {
   let move = 1
   let newIndex = undefined
   if(command === "move-left") move = -1
   if(command === "move-first") newIndex = 0
   if(command === "move-last") newIndex = -1
-  chrome.tabs.query( {active: true, currentWindow: true},([currentTab]) => {
+  browser.tabs.query( {active: true, currentWindow: true}).then( ([currentTab]) => {
     if(newIndex != undefined){
-      chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
+      browser.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
       return
     }
     if(currentTab){
-      chrome.tabs.getAllInWindow(currentTab.windowId, (tabs)=> {
+      browser.tabs.query({currentWindow: true}).then( (tabs)=> {
           newIndex = currentTab.index + move
           if(newIndex >= tabs.length) newIndex = 0
-          chrome.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
+          browser.tabs.move(currentTab.id, {windowId:currentTab.windowId, index: newIndex})
       })
     }
   })
